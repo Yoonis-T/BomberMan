9651a3ec7aad4c7afa7e2a146a6aa3e44dd42054
Abdulla Yoonis <yoonis.abdulla@edu.prakticum.fi>
2026-04-24 13:30:14 +0300

    Saving bomberman project

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public GameObject start;
+    public GameObject middle;
+    public GameObject end;
+
+    public void SetActiveRenderer(GameObject renderer)
+    {
+        start.SetActive(renderer == start);
+        middle.SetActive(renderer == middle);
+        end.SetActive(renderer == end);
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x);
+        transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+    }
+
+    public void DestroyAfter(float seconds)
+    {
+        Destroy(gameObject, seconds);
+    }
+}
